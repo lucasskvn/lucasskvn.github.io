7cbb065d437483017506450d1aa1c39c5a82e206
lucasskvn <lucas.sangkhavongs@epitech.eu>
2025-09-12 10:41:09 +0200

    init

---
+++ src/Contact.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div style={{ padding: "2rem", color: "#fff" }}>
+      <h1>Contact</h1>
+      <p>Vous pouvez me contacter à l'adresse : lucas.sangkhavongs@epitech.eu</p>
+    </div>
+  );
+}
