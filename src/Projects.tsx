8d7bfa74a22aaccc4883dbdaf202a2d13ee87aa0
LucquasSkvn <lucas.sangkhavongs@epitech.eu>
2025-09-12 13:20:44 +0200

    Updates

---
+++ src/Projects.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <div style={{ padding: "2rem", color: "#fff" }}>
+      <h1>Projects</h1>
+      <p>Voici quelques-uns de mes projets réalisés à EPITECH et en dehors.</p>
+    </div>
+  );
+}
