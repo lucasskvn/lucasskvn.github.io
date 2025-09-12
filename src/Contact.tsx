d18688b4a5c3b3cfcf2c8669f2c04ea0ce3913bc
LucquasSkvn <lucas.sangkhavongs@epitech.eu>
2025-09-12 13:53:31 +0200

    Updates

--- src/Contact.tsx
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-  return (
-    <div style={{ padding: "2rem", color: "#fff" }}>
-      <h1>Contact</h1>
-      <p>Vous pouvez me contacter à l'adresse : lucas.sangkhavongs@epitech.eu</p>
-    </div>
-  );
-}
